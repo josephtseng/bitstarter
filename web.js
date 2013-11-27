dffd501267ca11753cf107a9d86b6a824abd832f
Ubuntu <ubuntu@ip-172-31-10-146.us-west-2.compute.internal>
2013-11-27 07:48:57 +0000

    Debug index.html from disk

--- web.js
+++ web.js
@@ -4,7 +4,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-    var buf = new Buffer(8);
+    //var buf = new Buffer(8);
+    var buf;
     buf = fs.readFileSync('index.html');
     response.send(buf.toString());
 });
